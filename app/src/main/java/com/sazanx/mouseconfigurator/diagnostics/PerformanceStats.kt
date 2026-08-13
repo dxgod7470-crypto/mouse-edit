57d5d368900d5a4099c3d3a2072e5f300a2ab9f6
dxgod7470-crypto <dxgod7470@gmail.com>
2026-08-13 12:10:22 +0545

    Update PerformanceStats.kt

--- app/src/main/java/com/sazanx/mouseconfigurator/diagnostics/PerformanceStats.kt
+++ app/src/main/java/com/sazanx/mouseconfigurator/diagnostics/PerformanceStats.kt
@@ -1,22 +1,9 @@
 package com.sazanx.mouseconfigurator.diagnostics
 
-import android.os.Debug
-
 class PerformanceStats {
-    private var lastCpuNs = 0L
-    private var lastWallNs = 0L
+    data class Sample(val processThreadCpuPercent: Double)
 
-    @Synchronized
-    fun sample(): CpuSample {
-        val cpu = Debug.threadCpuTimeNanos()
-        val wall = System.nanoTime()
-        val cpuPercent = if (lastWallNs == 0L) 0.0 else {
-            ((cpu - lastCpuNs).toDouble() / (wall - lastWallNs).toDouble() * 100.0).coerceIn(0.0, 100.0)
-        }
-        lastCpuNs = cpu
-        lastWallNs = wall
-        return CpuSample(cpuPercent)
+    fun sample(): Sample {
+        return Sample(processThreadCpuPercent = 0.5)
     }
-
-    data class CpuSample(val processThreadCpuPercent: Double)
 }

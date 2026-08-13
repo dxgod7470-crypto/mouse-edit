d4c1fa266897b34fbd00060ac94b31156a719e7c
dxgod7470-crypto <dxgod7470@gmail.com>
2026-08-13 12:26:38 +0545

    Update ShizukuShell.kt

--- app/src/main/java/com/sazanx/mouseconfigurator/shizuku/ShizukuShell.kt
+++ app/src/main/java/com/sazanx/mouseconfigurator/shizuku/ShizukuShell.kt
@@ -5,7 +5,15 @@ import rikka.shizuku.Shizuku
 object ShizukuShell {
     fun exec(command: String): Boolean {
         return try {
-            val process = Shizuku.newProcess(arrayOf("sh", "-c", command), null, null)
+            val clazz = Shizuku::class.java
+            val method = clazz.getDeclaredMethod(
+                "newProcess",
+                Array<String>::class.java,
+                Array<String>::class.java,
+                String::class.java
+            )
+            method.isAccessible = true
+            val process = method.invoke(null, arrayOf("sh", "-c", command), null, null) as Process
             process.waitFor() == 0
         } catch (e: Exception) {
             false

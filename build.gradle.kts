89474618f66408b1ce90adca2e9ce39f4788ebe6
dxgod7470-crypto <dxgod7470@gmail.com>
2026-08-13 11:53:53 +0545

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}

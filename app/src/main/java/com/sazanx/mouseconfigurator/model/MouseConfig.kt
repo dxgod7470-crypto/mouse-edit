55da89307b2bc5158cf1dd333cc6918808de6e09
dxgod7470-crypto <dxgod7470@gmail.com>
2026-08-13 12:05:59 +0545

    Update MouseConfig.kt

--- app/src/main/java/com/sazanx/mouseconfigurator/model/MouseConfig.kt
+++ app/src/main/java/com/sazanx/mouseconfigurator/model/MouseConfig.kt
@@ -4,11 +4,11 @@ data class MouseConfig(
     val pointer: Float = 1.0f,
     val x: Float = 1.0f,
     val y: Float = 1.0f,
+    val raw: Boolean = true,
     val acceleration: Boolean = false,
     val accel: Float = 0.0f,
-    val smoothing: Float = 0.35f,
+    val smoothing: Float = 0.0f,
     val curve: String = "Linear",
-    val raw: Boolean = true,
     val scroll: Float = 1.0f,
     val invertY: Boolean = false
 )

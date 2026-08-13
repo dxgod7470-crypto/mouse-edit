65f8b8e1f3d16288301b8b904828994ca413dd42
dxgod7470-crypto <dxgod7470@gmail.com>
2026-08-13 16:48:59 +0545

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,14 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = java.net.URI("https://jitpack.io") }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
